25679a8d2e71b4bc8e28e06228b9b18383cc633e
yssu <ky200120000@gmail.com>
2018-06-01 12:23:09 +0800

    update

--- push.sh
+++ push.sh
@@ -4,8 +4,8 @@ git add README.md
 git add script
 git add solution 
 git add submit
-git add push.sh
+#git add push.sh
 git add .gitignore
 #git add
-git commit -m "v1"
+git commit -am "update"
 git push --all
