7ccdb88f7b7f39d548f7b6106c55c335b027e232
yssu <ky200120000@gmail.com>
2018-06-01 22:23:37 +0800

    update

--- script/fasttest_lgbm.py
+++ script/fasttest_lgbm.py
@@ -1,13 +1,21 @@
 import pandas as pd
+import lightgbm as lgb
+
 
 train_csv='../input/train.csv'
 test_csv='../input/test.csv'
+fasttest_train='fasttest_train.csv'
+fasttext_test='fasttext_test.csv'
 
-df_train = pd.read_csv(train_csv)
-df_train = df_train[['item_id','user_id','deal_probability']]
+#df_train = pd.read_csv(train_csv)
+#df_train = df_train[['item_id','user_id','deal_probability']]
+df_fasttext_train = pd.read_csv(fasttext_train,sep=' ',names = range(300)) 
 
-df_test = pd.read_csv(test_csv)
-df_test = df_test[['item_id','user_id']]
+#df_test = pd.read_csv(test_csv)
+#df_test = df_test[['item_id','user_id']]
+df_fasttext_test = pd.read_csv(fasttext_test,sep=' ',names = range(300)) 
 
-print(df_train.head(2))
-print(df_test.head(2))
+#print(df_train.head(2))
+#print(df_test.head(2))
+print(df_fasttext_train.head(1))
+print(df_fasttext_test.head(1))
